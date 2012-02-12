dde03501376e699102553ee47f8e79c2835c7ea0
lorenzo puccetti <lorenzo.puccetti@gmail.com>
2012-02-12 13:05:04 +0000

    minor editing + standard free license.

--- getsomefeedback.js
+++ getsomefeedback.js
@@ -1,4 +1,4 @@
-// getfeedback.js
+// getsomefeedback.js
 // february, 2012. 
 // 
 // Copyright (c) 2012 lorenzo puccetti
@@ -13,7 +13,7 @@
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
 //
-// Finally, this Software shall be used for doing good things, not bad things.
+// This Software shall be used for doing good things, not bad things.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
